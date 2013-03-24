397fc27a2e5cb58b99d5506197173610a0131435
Josh Gillham <usajoshgillham@gmail.com>
2013-03-24 16:16:59 -0600

    testnodes: updated tests.

--- pathfinding/testnodes.py
+++ pathfinding/testnodes.py
@@ -1,6 +1,7 @@
 import unittest
 import graph
 import nodes
+import math
 
 class TestNodes( unittest.TestCase ):
     def test_ExplorePath_Simple( self ):
@@ -8,18 +9,23 @@ class TestNodes( unittest.TestCase ):
         Checks to see if explorePath can find its way from one point to another.
         """
         links = []
-        n1 = graph.Node( 10, 50 )
-        n2 = graph.Node( 10, 50 )
-        n3 = graph.Node( 10, 50 )
-        n7 = graph.Node( 10, 50 )
+        n1 = graph.Node( 10, 10 )
+        n2 = graph.Node( 10, 20 )
+        n3 = graph.Node( 10, 30 )
+        n7 = graph.Node( 10, 40 )
+        d = 0
+        d = d + math.hypot( n1.X - n2.X, n1.Y - n2.Y )
+        d = d + math.hypot( n2.X - n3.X, n2.Y - n3.Y )
+        d = d + math.hypot( n3.X - n7.X, n3.Y - n7.Y )
 
         links.append( graph.Link( n1, n2 ) )
         links.append( graph.Link( n2, n3 ) )
         links.append( graph.Link( n3, n7 ) )
         roots = [n1]
         actual = nodes.explorePath( links, roots, n1 )
-        expected = [ n1, n2, n3, n7 ]
-        self.assertEqual( expected, actual )
+        expected = ( [ n1, n2, n3, n7 ], d )
+        self.assertEqual( expected[0], actual[0] )
+        self.assertEqual( expected[1], actual[1] )
     def test_ExplorePath( self ):
         """
         Checks to see if explorePath can find its way from a path that
@@ -35,7 +41,11 @@ class TestNodes( unittest.TestCase ):
         n5b = graph.Node( 15, 50 )
         n6a = graph.Node( 5, 60 )
         n6b = graph.Node( 15, 60 )
-        n7 = graph.Node( 10, 70 )
+        d = 0
+        d = d + math.hypot( n1.X - n2.X, n1.Y - n2.Y )
+        d = d + math.hypot( n2.X - n3.X, n2.Y - n3.Y )
+        d = d + math.hypot( n3.X - n4b.X, n3.Y - n4b.Y )
+        d = d + math.hypot( n4b.X - n5b.X, n4b.Y - n5b.Y )
 
         links.append( graph.Link( n1, n2 ) )
         links.append( graph.Link( n2, n3 ) )
@@ -44,12 +54,11 @@ class TestNodes( unittest.TestCase ):
         links.append( graph.Link( n4a, n5a ) )
         links.append( graph.Link( n4b, n5b ) )
         links.append( graph.Link( n5a, n6a ) )
-        links.append( graph.Link( n6a, n7 ) )
-        links.append( graph.Link( n5b, n7 ) )
         roots = [n1]
-        actual = nodes.explorePath( links, n1, n1 )
-        expected = [ n1, n2, n3, n4b, n5b, n7 ]
-        self.assertEqual( expected, actual )
+        actual = nodes.explorePath( links, roots, n1 )
+        expected = ( [ n1, n2, n3, n4b, n5b ], d )
+        self.assertEqual( expected[0], actual[0] )
+        self.assertEqual( expected[1], actual[1] )
     def test_findLinksWithNode( self ):
         links = []
         n1 = graph.Node( 10, 10 )
