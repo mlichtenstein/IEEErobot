a5c5089931fec930636f8992860aeb112617e50c
Matthew Bird <birdmw@gmail.com>
2013-03-27 21:31:15 -0600

    pathfinder is complete as a gui

--- pathfinding/edit.py
+++ pathfinding/edit.py
@@ -1,4 +1,4 @@
-import easygui
+import easygui, math
 from easygui import *
 import sys
 
@@ -19,9 +19,9 @@ def editNode(node):
     return True
 
 def editLink(link):
-    msg = "Link " + str(link)
+    msg = link.node1v+" "+link.node1h+" nodes angle:" + str(-int(180/math.pi*math.atan2(link.node2.Y-link.node1.Y,link.node2.X-link.node1.X)))+"           "+ link.node2v+" "+link.node2h+" nodes angle:" +str(-int(180/math.pi*math.atan2(link.node1.Y-link.node2.Y,link.node1.X-link.node2.X)))
     title = "Edit link " + str(link) +"'s data"
-    fieldNames = ["crossed log (0/1)", link.node1h+" "+link.node1v+" nodes direction", link.node2h+" "+link.node2v+" nodes direction", "length", "Red", "Green", "Blue"]
+    fieldNames = ["crossed log (0/1)", link.node1v+" "+link.node1h+" nodes angle", link.node2v+" "+link.node2h+" nodes angle", "length", "Red", "Green", "Blue"]
     fieldValues = [link.log, link.node1direction, link.node2direction, link.length, link.red, link.green, link.blue]
     fieldValues = multenterbox(msg,title, fieldNames, fieldValues)
     if fieldValues == None:
