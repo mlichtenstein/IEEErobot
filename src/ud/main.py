cac132076a7ba6574da55d18c5c0c2e2f16b50ff
Josh Gillham <usajoshgillham@gmail.com>
2013-04-01 20:28:25 -0600

    Upper Deck: Go and main handle interrupts.

--- src/ud/main.py
+++ src/ud/main.py
@@ -13,6 +13,8 @@ import world
 import time
 import random
 
+
+
 """=================SETUP============================================"""
 
 #give the robot a state:
@@ -20,8 +22,23 @@ state = robotbasics.State()
 #establish a serial connection that will persist into modes.py:
 modes.Mode.messenger = messenger.Messenger(messenger.SerialPort())
 
+def signalNewMode( newMode ):
+    if nextMode != robotMode:
+        nextMode = newMode
+def switchMode():
+    if nextMode != None:
+        robotMode.end()
+        robotMode = newMode
+        nextmode = None
+        robotMode.begin()
+# Share the function used to change the states.
+modes.Mode.signalNewMode = switchMode
+
 #pick a start mode (should be wait, eventually)
 robotMode = modes.ReadUSBDrive(state)
+nextMode = None
+
+
 
 #create world
 landmarkList = world.World.landmarkList
@@ -48,6 +65,7 @@ random.seed(time.time())
 """===============MAIN LOOP=========================================="""
 running = True
 
+robotMode.begin()
 while running == True:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -73,8 +91,28 @@ while running == True:
     print state.pose
     if state.paused == False:
         nextMode = robotMode.act(state)
-        if nextMode != None:
-            robotMode = nextMode
+    signalNewMode( nextMode )
+    
+    #-------------------------------------------------------------------------
+    # BEGIN Event Driven Architecture
+    #
+    # This section should handle incoming messages from the Arduino. Well
+    #  designed code should never have a loop in the robot modes which check
+    #  messages. Instead, implement event driven architecture here. Event driven
+    #  architecture is very robust and can allow for interrupts.
+    
+    # Get messages from the Arduino.
+    if modes.Mode.messenger.checkInBox():
+        message = modes.Mode.messenger.getMessageTuple()
+        category = message[0]
+        # Trigger the confirmation event in the robot mode.
+        if category == SERIAL_MESSAGE_CONFIRMATION:
+            robotMode.onConfirmation( message[1] )
+        else:
+            robotMode.onMessage( message )
+    
+    # END Event Driven Architecture
+    #-------------------------------------------------------------------------
     
     # Update the GUI with the current robot state
     state = gui.takeState(state)
@@ -82,3 +120,4 @@ while running == True:
     gui.update(screen)
     pygame.display.update()
     time.sleep(.3)
+    switchMode()
